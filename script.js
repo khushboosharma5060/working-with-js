617e7cdf22f7beb952bb9f427b34ba680a120093
Khushboo Sharma <khushboosharma5060@gmail.com>
2023-04-11 18:26:19 +0400

    working with js

---
+++ script.js
@@ -0,0 +1,18 @@
+
+const addNumAer = ((num1, num2) => {
+
+     return num1+ num2;
+})
+const sum = addNumAer(5, 6);
+console.log(sum);
+
+
+
+
+
+const multiplyNumAer = ((num1, num2) => {
+
+     return num1 * num2;
+})
+const result = multiplyNumAer(5, 6);
+console.log(result);
